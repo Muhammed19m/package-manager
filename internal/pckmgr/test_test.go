84a4913c47ec7a09c1e30bde8908946173e10252
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-07-25 22:12:17 +0300

    добавить пару тестов UpdatePackages

--- internal/pckmgr/test_test.go
+++ internal/pckmgr/test_test.go
@@ -9,10 +9,10 @@ import (
 func Test_CreatePackage(t *testing.T) {
 	t.Run("Name должен быть валидным", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-		Name:     "",
-		Ver:      "",
-		Targets:  []Target{},
-		Packages: []PackageDependency{},
+			Name:     "",
+			Ver:      "",
+			Targets:  []Target{},
+			Packages: []PackageDependency{},
 		})
 
 		assert.Error(t, err)
@@ -20,15 +20,15 @@ func Test_CreatePackage(t *testing.T) {
 
 	t.Run("Ver должен быть валидным", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-		Name:     "",
-		Ver:      "",
-		Targets:  []Target{},
-		Packages: []PackageDependency{},
+			Name:     "",
+			Ver:      "",
+			Targets:  []Target{},
+			Packages: []PackageDependency{},
 		})
 
 		assert.Error(t, err)
 	})
-	
+
 	t.Run("Targets не должен быть пустым", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
 			Name:     "",
@@ -40,12 +40,14 @@ func Test_CreatePackage(t *testing.T) {
 		assert.Error(t, err)
 	})
 
-	t.Run("успешная загрузка по строковому шаблону", func(t *testing.T) {
+	t.Run("успешная загрузка по шаблону", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-			Name:     "package-1",
-			Ver:      "1.0",
-			Targets:  []Target{
-				"./funny.png",
+			Name: "package-1",
+			Ver:  "1.0",
+			Targets: []Target{
+				{
+					Path: "./funny.png",
+				},
 			},
 			Packages: []PackageDependency{},
 		})
@@ -53,14 +55,14 @@ func Test_CreatePackage(t *testing.T) {
 		assert.NoError(t, err)
 	})
 
-	t.Run("успешная загрузка по расширенному шаблону", func(t *testing.T) {
+	t.Run("успешная загрузка по шаблону с исключением", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-			Name:     "package-1",
-			Ver:      "1.0",
-			Targets:  []Target{
-				TargetExtended{
-					Path: "./funny*.png",
-					Exclude: "",
+			Name: "package-1",
+			Ver:  "1.0",
+			Targets: []Target{
+				Target{
+					Path:    "./funny*.png",
+					Exclude: "*exluded.png",
 				},
 			},
 			Packages: []PackageDependency{},
@@ -71,10 +73,12 @@ func Test_CreatePackage(t *testing.T) {
 
 	t.Run("успешная загрузка вместе с зависимостью", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-			Name:     "package-1",
-			Ver:      "1.0",
-			Targets:  []Target{
-				"./funny.png",
+			Name: "package-1",
+			Ver:  "1.0",
+			Targets: []Target{
+				Target{
+					Path: "./funny*.png",
+				},
 			},
 			Packages: []PackageDependency{
 				{Name: "package-3", Ver: "<=2.0"},
@@ -86,10 +90,12 @@ func Test_CreatePackage(t *testing.T) {
 
 	t.Run("зависимость должна существовать", func(t *testing.T) {
 		err := CreatePackage(PackageForCreate{
-			Name:     "package-1",
-			Ver:      "1.0",
-			Targets:  []Target{
-				"./funny.png",
+			Name: "package-1",
+			Ver:  "1.0",
+			Targets: []Target{
+				Target{
+					Path: "./funny*.png",
+				},
 			},
 			Packages: []PackageDependency{
 				{Name: "package-not-exits", Ver: "<=2.0"},
@@ -100,19 +106,29 @@ func Test_CreatePackage(t *testing.T) {
 	})
 }
 
-func CreatePackage(a PackageForCreate) error{return nil}
+func CreatePackage(a PackageForCreate) error { return nil }
 
-func Test_UpdatePackage(t *testing.T) {
-	t.Run("Name должен быть валидным", func(t *testing.T) {
-		err := UpdatePackage(PackagesForUpdate{
-		Name:     "",
-		Ver:      "",
-		Targets:  []Target{},
-		Packages: []PackageDependency{},
+func Test_UpdatePackages(t *testing.T) {
+	t.Run("Packages не должно быть пустым", func(t *testing.T) {
+		err := UpdatePackages(PackagesForUpdate{
+			Packages: []PackageRequest{},
 		})
 
 		assert.Error(t, err)
 	})
+
+	t.Run("Пакет успешно загружен и существет в директории", func(t *testing.T) {
+		expectedFile := "./funny1.png"
+		err := UpdatePackages(PackagesForUpdate{
+			Packages: []PackageRequest{{
+				Name: "package-1",
+				Ver:  "1.0",
+			}},
+		})
+		
+		assert.FileExists(t, expectedFile)
+		assert.NoError(t, err)
+	})
 }
 
-func UpdatePackage(a PackagesForUpdate) error {return nil}
+func UpdatePackages(a PackagesForUpdate) error { return nil }
