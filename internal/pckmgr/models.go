a7301e783277787585907112a3b3df46046b9536
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-07-25 21:58:15 +0300

    добавить комментарии к моделям

--- internal/pckmgr/models.go
+++ internal/pckmgr/models.go
@@ -2,35 +2,35 @@ package pkgmgr
 
 //  Создание архивов
 
+// PackageForCreate представляет собой мета информацию о пакете для создания архива и отправки его на сервер
 type PackageForCreate struct {
-	Name string
-	Ver string
-	Targets []Target
- 	Packages []PackageDependency
+	Name     string
+	Ver      string
+	Targets  []Target
+	Packages []PackageDependency
 }
 
-type Target any // Может быть строкой или типом TargetExtended
-
-type TargetExtended struct {
-	Path string
-	Exclude string
+// Target это шаблон файлов для создания пакетов
+type Target struct {
+	Path    string
+	Exclude string // Опциональное поле. Содержит шаблон исключения
 }
 
+// PackageDependency представляет с собой информацию о зависимости пакета
 type PackageDependency struct {
 	Name string
-	Ver string
+	Ver  string
 }
 
 //  Загрузка архивов
 
+// PackagesForUpdate представляет собой запрос пакетов к скачиванию
 type PackagesForUpdate struct {
 	Packages []PackageRequest
 }
 
+// PackageRequest пакет
 type PackageRequest struct {
 	Name string
-	Ver string
+	Ver  string
 }
-
-
-
