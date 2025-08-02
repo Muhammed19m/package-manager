a2ba649576c7b0bb486f6db8dedde60b0e00f51d
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-08-02 13:27:23 +0300

    добавить недоделанную распаковку

--- internal/pkgmgr/update_packages.go
+++ internal/pkgmgr/update_packages.go
@@ -1,11 +1,18 @@
 package pkgmgr
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"io"
 	"net"
 	"os"
 	"path"
+	"path/filepath"
+	"runtime"
+	"time"
 
+	"github.com/mholt/archives"
 	"github.com/povsister/scp"
 )
 
@@ -30,12 +37,53 @@ func UpdatePackages(a UpdatePackagesIn) error {
 	}
 	defer scpClient.Close()
 
+	tmpDir := filepath.Join(os.TempDir(), "pkgmgr", fmt.Sprint(time.Now().Unix()))
+	if err := os.MkdirAll(tmpDir, 0755); err != nil {
+		return err
+	}
+
 	for _, pkg := range a.Packages {
 
 		archiveName := pkg.Name + "-" + pkg.Ver + ".tar"
 		remoteTargetAbs := path.Join(a.SshConfig.PackagesDir, archiveName)
 
-		if err := scpClient.CopyFileFromRemote(remoteTargetAbs, a.DownloadDir, &scp.FileTransferOption{}); err != nil {
+		if err := scpClient.CopyFileFromRemote(remoteTargetAbs, tmpDir, &scp.FileTransferOption{}); err != nil {
+			return err
+		}
+
+		input, err := os.Open(filepath.Join(tmpDir, archiveName))
+		if err != nil {
+			return err
+		}
+		defer input.Close()
+
+		var format archives.Tar
+
+		err = format.Extract(context.TODO(), input, func(ctx context.Context, f archives.FileInfo) error {
+			archiveFile, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer archiveFile.Close()
+
+			nameInArchive := f.NameInArchive
+			if runtime.GOOS == "windows" {
+				nameInArchive = filepath.FromSlash(nameInArchive)
+			}
+
+			newFile, err := os.Create(filepath.Join(a.DownloadDir, nameInArchive))
+			if err != nil {
+				return err
+			}
+			defer newFile.Close()
+
+			if _, err = io.Copy(newFile, archiveFile); err != nil {
+				return err
+			}
+
+			return nil
+		})
+		if err != nil {
 			return err
 		}
 	}
